ca1cbd6f98aac6f495b9b6ba2685d9c5ab8c1076
jericodellomas <jericodellomas23@gmail.com>
2023-10-06 10:36:40 +0800

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,16 @@
-<template> </template>
+<script setup>
+import {ref} from 'vue' 
+
+const input = ref('')
+</script>
+<template>
+    <select v-model="input">
+        <option value="html">HTML</option>
+        <option value="css">CSS</option>
+    </select>
+    <p> p:{{ input }}</p>
+
+    <!-- Conditional--> 
+    <p v-if="input == 'html'"> Hypertext Markup Language</p>
+    <p v-else-if="input == 'css'"> Cascading Style Sheets</p>
+</template>
